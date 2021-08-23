ca6d6e226f7474b56d0074f621aba11499fd2f35
nhuse <nathan.huse@yahoo.com>
2021-08-23 17:40:28 -0500

    More login progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,30 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+  enable :sessions
   
   # Add your routes here
   get "/" do
     { message: "Good luck with your project!" }.to_json
+  end
+
+  get '/drinks' do 
+    drinks = Drink.all
+    drinks.to_json
+  end
+
+  get '/drinks/:id' do
+    drink = Drink.find(params[:id])
+    drink.to_json
+  end
+
+  get '/cold_drinks' do
+    cold_drinks = Drink.where(category: 'Cold Coffee')
+    cold_drinks.to_json
+  end
+
+  get '/hot_drinks' do
+    hot_drinks = Drink.where(category: 'Hot Coffee')
+    hot_drinks.to_json
   end
 
 end
--- my-app-frontend/src/components/Navbar.js
+++ my-app-frontend/src/components/Navbar.js
@@ -2,8 +2,11 @@ import { Route, Switch, Link } from "react-router-dom";
 import LogInPage from "./LogInPage"
 function Navbar() {
     return(
-
-       <LogInPage/>
+        <header>
+            <nav>
+                <LogInPage />
+            </nav>
+        </header>
     )
 }
 export default Navbar;
