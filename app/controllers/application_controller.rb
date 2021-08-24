d71f854708116b6586b447e0b6a399956b45e882
leslieyi <leslieyi411@gmail.com>
2021-08-24 10:14:41 -0400

    day 2 morning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+  register Sinatra::CrossOrigin
+  enable :sessions
+
+  configure do
+    enable :cross_origin
+    set :allow_origin, "*" 
+    set :allow_methods, [:get, :post, :patch, :delete, :options] # allows these HTTP verbs
+    set :expose_headers, ['Content-Type']
+  end
+
+  options "*" do
+    response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+    200
+  end
+  
   
   get "/" do
     { message: "Good luck with your project!" }.to_json
@@ -8,6 +24,7 @@ class ApplicationController < Sinatra::Base
   get '/drinks' do 
     drinks = Drink.all
     drinks.to_json
+    #this will show the categories with the ID, and you can get the name of the Categories through the macros
   end
 
   get '/drinks/:id' do
---
+++ app/controllers/customer_controller.rb
@@ -0,0 +1,37 @@
+class CustomerController < ApplicationController
+  
+    post '/login' do
+        customer = Customer.find_by(email: "#{login_params["email"]}")
+        puts customer
+        if customer && customer.password == login_params["password"]
+            session[:customer] = customer.id
+            customer.to_json
+        else
+            {error: "Wrong credentials or email is not in use. Please try again."}.to_json
+        end
+    end
+  
+    post '/register' do
+        # register_params
+        email = register_params["email"]
+        if Customer.find_by(email: "#{register_params["email"]}")
+            {error: "Email is already in use. Please login or use a different email"}.to_json
+        else
+            Customer.create(register_params)
+            {success: "Successfully created account"}.to_json
+        end
+    end
+
+
+
+    private
+    def register_params
+        allowed_params = %w(name email password)
+        params.select {|param,value| allowed_params.include?(param)}
+    end
+
+    def login_params
+        allowed_params = %w(email password)
+        params.select {|param,value| allowed_params.include?(param)}
+    end
+end
