ecb2aabd4d42feeb8c0392c2fbf94cea23307cef
nhuse <nathan.huse@yahoo.com>
2021-08-26 22:40:16 -0500

    Merge with main

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,4 @@
 class Customer < ActiveRecord::Base
-<<<<<<< HEAD
-  has_many :orders 
-  has_many :drinks, through: :orders
-=======
     has_many :orders 
     has_many :drinks, through: :carts
->>>>>>> main
 end
